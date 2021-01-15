8f06b6e74cee2062e3f0f71cfcd674080297633d
Jabinator1 <jabinator1@gmail.com>
2021-01-14 19:16:03 -0700

    added meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,47 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Josh Berube</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="Josh Berube's Portfolio" />
+        <meta 
+          name="description" 
+          content="I'm Josh Berube, a web developer based in Lehi, Utah. I create beautiful, optimized, and clean websites."
+        />
+        <meta name="keywords" content="Josh Berube, Josh, Berube, Josh Berube Portfolio, Portfolio, Web Developer, Josh Berube Web Developer" />
+        <meta name="author" content="Josh Berube" />
+
+        <meta property="og:site_name" content="Josh Berube" />
+        <meta property="og:url" content="https://joshberube.com" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Hi, I'm Josh." />
+        <meta 
+          property="og:description" 
+          content="I'm Josh Berube, a web developer based in Lehi, Utah. I create beautiful, optimized, and clean websites."
+        />
+        <meta property="og:image" content="https://joshberube.com/profileOGImage.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <h1>Josh Berube</h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      fontFamily: {
+        'proxima-nova': ["Proxima Nova"]
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
